f1ed0fe06472eaa881743dadcf295e9d08ea3c6b
rimanshupatel <rimanshupatel1@gmail.com>
2025-01-18 10:02:58 +0530

    adding logic for array must have 3 element

--- Array/ProductTriplet.java
+++ Array/ProductTriplet.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
-public class Demo {
+public class ProductTriplet {
 
     public static int MaxProduct(int[] arr) {
+        if (arr.length < 3) {
+            System.out.println("Array must have at least 3 elements");
+        }
+
         Arrays.sort(arr);
         int n = arr.length;
+
         int Product_1 = arr[n - 1] * arr[n - 2] * arr[n - 3];
         int Product_2 = arr[0] * arr[1] * arr[n - 1];
         return Math.max(Product_1, Product_2);
